ac33f18f5cfaff3eac1f593899205f8ffd6ffd8c
Matheus Saraiva <saraivarego@gmail.com>
2023-07-24 18:45:09 -0300

    from django.conf import settings

--- jornada/urls.py
+++ jornada/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -6,7 +8,9 @@ from depoimentos.views import DepoimentosViewSet
 
 router = routers.DefaultRouter()
 router.register('depoimentos', DepoimentosViewSet)
+router.register('depoimentos-home', DepoimentosViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
